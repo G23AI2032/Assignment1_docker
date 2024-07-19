6968ced0a5cf73ebf5d4f314879faeb8506ab783
G23AI2032 <g23ai2032@iitj.ac.in>
2024-07-20 00:39:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+feedback_data = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit_feedback', methods=['POST'])
+def submit_feedback():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    message = request.form['message']
+    
+   
+    feedback_data.append({
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'message': message
+    })
+    
+    return redirect(url_for('thank_you'))
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
+
+if __name__ == '__main__':
+    app.run(debug=True , host='0.0.0.0')
+
